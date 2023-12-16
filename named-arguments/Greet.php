b186882d0363ccbd59538872e80f39ce103498c0
Iloba <ilobatimothy@gmail.com>
2023-12-16 09:25:55 +0100

    added files for named arguments tutorials

--- named-arguments/Greet.php
+++ named-arguments/Greet.php
@@ -4,20 +4,23 @@ class Greet
 {
 
     //Our first method
-//    public function sayHello($name){
+//    public function sayHello(string $name): void
+//    {
 //        print "Hello " . $name;
 //    }
 //
 
 //Our second method
 
-//    public function sayHello($name, $message = ' you are welcome'){
+//    public function sayHello(string $name, string $message = ' you are welcome'): void
+//    {
 //         print "Hello " . $name . $message;
 //    }
 
 
 //Our Final Method
-    public function sayHello($name, $message = ' you are welcome', $billingMessage = ' your total charge is $200'){
+    public function sayHello(string $name, string $message = ' you are welcome', string $billingMessage = ' your total charge is $200'): void
+    {
         print "Hello " . $name . $message . $billingMessage;
     }
 }
--- named-arguments/practice.php
+++ named-arguments/practice.php
@@ -5,12 +5,8 @@ require 'Greet.php';
 $app = new Greet();
 
 //Without Named Arguments
-//$app->sayHello(name: "John",  billingMessage: "your total charge is $300");
-
-
-//Without Named Arguments
 //$app->sayHello("John", " welcome to our app", " your total charge is $300");
 
 
 //With Named Arguments
-$app->sayHello(name: "John",  billingMessage: " your total charge is $300");
+$app->sayHello(name: "John", message: "welcome to our app", billingMessage: " your total charge is $300");
